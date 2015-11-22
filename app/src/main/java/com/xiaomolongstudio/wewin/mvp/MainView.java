e3fed6fc3c8a33edbe88ffe673bbfafa26eb3ae1
小尛龙 <1413129987@qq.com>
2015-11-22 23:02:24 +0800

    showImage

---
+++ app/src/main/java/com/xiaomolongstudio/wewin/mvp/MainView.java
@@ -0,0 +1,14 @@
+package com.xiaomolongstudio.wewin.mvp;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2015/11/22 0022.
+ */
+public interface MainView {
+    void showData(List<MainModel> mainList);
+
+    void showProgress();
+
+    void hideProgress();
+}
